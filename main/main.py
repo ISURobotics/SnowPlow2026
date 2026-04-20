21169fbf39288201e979f666ad6e8a81ed7d9e08
Joshua Watters <129793528+joshwatters@users.noreply.github.com>
2026-04-20 07:35:41 -0500

    Update main.py

--- main/main.py
+++ main/main.py
@@ -20,20 +20,6 @@ cone3hypotenuse=0
 # degrees between -90 and positive 90. Straight forward is 0. hypotenuse represents distance to cone.
 cones_are_at=[[cone1degrees,cone1hypotenuse],[cone2degrees,cone2hypotenuse],[cone3degrees,cone3hypotenuse]]
 
-#####################################################################################################################
-# Full testing of this code requires that ROS2 be running on the network. In it's fully enabled version,
-# it reads from a controller, applies mathematical functions and broadcasts the motor position in an array.
-# If enabled, this would start motor movement. To prevent unexpected movement, portions are disabled by default.
-# A message is displayed to show that the micro_service.py file is connected. It can be modded at the respective file location.
-# This program has been implemented as microservices. Examples of this are in the below paragraph.
-# It is able to broadcast data via ROS topic or log to websocket. The format used was the standardized format we use with NVIDIA
-# frameworks: When sending a packet to isaac sim all motor positions are sent in a standard array format in degrees:
-# [j0,j1,j2,...]. When using sensors a second packet is created and can be published to ROS2 topic as well as logged to a file
-# for publishing to websocket. In many cases Data is "returned" for immediate use and pushed to ROS for use in Isaac Simulator.
-# This initial portion of the code handles reading from controller and getting data to ROS2.
-# This currently requires ROS2 running on the local network. Bridge to API to follow.
-#####################################################################################################################
-
 # Used to organize detected sensors
 sensor_array = [0,0,0,0,0,0,0]
 
